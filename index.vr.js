a5db2e1c33230a93980d2e51f683c8cfb4139ba5
Srivatsa S <srivatsashankaran@gmail.com>
2017-08-24 00:28:34 +0530

    Structure 2D Content like Images and Text using the View component in React VR

--- index.vr.js
+++ index.vr.js
@@ -1,27 +1,69 @@
 import React from 'react';
-import { AppRegistry, View, Pano, asset, Text } from 'react-vr';
+import { AppRegistry, View, Pano, asset, Text, Image } from 'react-vr';
 
 export default class App extends React.Component{
   render() {
     return(
       <View>
         <Pano source={asset('chess-world.jpg')} />
-        <Text
+        <View
           style={{
-            transform: [{ translateZ: -1 }],
-            color: 'lightblue',
-            backgroundColor: '#355',
-            fontSize: 0.1,
-            fontWeight: 300,
-            width: 0.55,
-            height: 0.45,
-            textAlign: 'center',
-            textAlignVertical: 'center',
-            layoutOrigin: [0.5,0.5]
+            width: 2,
+            height: 2.4,
+            backgroundColor: 'white',
+            layoutOrigin: [0.5, 0.5],
+            transform: [{ translate: [0, 0,-3]}],
+            justifyContent: 'space-between'
           }}
         >
-          Srivatsa
-        </Text>
+          <Image 
+            source={asset('mountains.jpg')}
+            style={{
+              height: 1.2
+            }}
+          />
+          <Text
+            style={{
+              color: '#333',
+              fontSize: 0.16,
+              textAlign: 'center'
+            }}
+          >
+            Explore Austria with wonderful Ski
+          </Text>
+          <View 
+            style={{flexDirection: 'row'}}
+          >
+            <Image 
+              source={asset('mountains1.jpg')}
+              style={{
+                width: 0.5,
+                height: 0.5
+              }}
+            />
+              <Image 
+              source={asset('mountains2.jpg')}
+              style={{
+                width: 0.5,
+                height: 0.5
+              }}
+            />
+            <Image 
+              source={asset('mountains3.jpg')}
+              style={{
+                width: 0.5,
+                height: 0.5
+              }}
+            />
+            <Image 
+              source={asset('mountains4.jpg')}
+              style={{
+                width: 0.5,
+                height: 0.5
+              }}
+            />
+          </View>
+        </View>
       </View>
     )
   }
