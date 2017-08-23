a7c8d7c88190d8ce988abb25efa43b3194c821f4
Srivatsa S <srivatsashankaran@gmail.com>
2017-08-24 00:11:42 +0530

    Writing text using the Text component in React VR

--- index.vr.js
+++ index.vr.js
@@ -1,17 +1,27 @@
 import React from 'react';
-import { AppRegistry, View, Pano, asset } from 'react-vr';
+import { AppRegistry, View, Pano, asset, Text } from 'react-vr';
 
 export default class App extends React.Component{
   render() {
     return(
       <View>
-        <Pano 
-        source={asset('read-world.jpg')}
-        onLoad={() => console.log('Loaded')}
-        style={{
-          transform: [{ rotateY: -20}]
-        }}
-        />
+        <Pano source={asset('chess-world.jpg')} />
+        <Text
+          style={{
+            transform: [{ translateZ: -1 }],
+            color: 'lightblue',
+            backgroundColor: '#355',
+            fontSize: 0.1,
+            fontWeight: 300,
+            width: 0.55,
+            height: 0.45,
+            textAlign: 'center',
+            textAlignVertical: 'center',
+            layoutOrigin: [0.5,0.5]
+          }}
+        >
+          Srivatsa
+        </Text>
       </View>
     )
   }
